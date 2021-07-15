bd053263efc11a077122190e83e716e55a85598b
unknown <niranjanielumalai4>
2021-07-15 21:01:28 +0530

    LINQ task

---
+++ Linq_Assignment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Linq_Assignment.MvcApplication" Language="C#" %>
---
+++ Linq_Assignment/Models/ProductsReport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace Linq_Assignment.Models
+
+{
+    public class ProductsReport
+    {
+        public DateTime Date { get; set; }
+        public Product ProductName { get; set; }
+        public decimal? Quantity { get; set; }
+    }
+}
